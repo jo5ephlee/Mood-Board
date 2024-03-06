f01f1c24b4650b1ca108e45a9fddb4ff9e31ca90
eddyc-j <edward.cho.jung@gmail.com>
2024-03-05 20:12:21 -0500

    authentication variables

--- main-env/main.py
+++ main-env/main.py
@@ -1,5 +1,11 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="0f3a303624e946d8bcab3c68608cf01e",
+                                               client_secret="9fff5a85fc3d416a94a18cf031d1484c",
+                                               redirect_uri="https://localhost:8888/callback",
+                                               scope="user-library-read"))
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
